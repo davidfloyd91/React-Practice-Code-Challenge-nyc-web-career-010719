1c0381ebc03472825b3ec2c0c4c90e6438bf7b24
davidfloyd91 <davidfloyd91@gmail.com>
2019-03-19 12:33:29 -0400

    Arguably did the things. More arguably did them badly.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,25 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    wallet: 100,
+  };
+
+  handleEat = sushi => {
+    if (this.state.wallet >= sushi.price) {
+      this.setState({
+        wallet: this.state.wallet - sushi.price,
+      });
+    } else {
+      alert("Sorry dog you're broke.");
+    };
+  };
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer onEat={this.handleEat} />
+        <Table wallet={this.state.wallet} />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,68 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
-}
+class SushiContainer extends Component {
+  state = {
+    sushi: [],
+    batchIndex: 0,
+  };
 
-export default SushiContainer
+  componentDidMount() {
+    this.setState({
+      sushi: [],
+    }, () =>  {
+      fetch("http://localhost:3000/sushis")
+      .then(r => r.json())
+      .then(sushi => {
+        sushi.forEach(s => {
+          this.setState({
+            sushi: [...this.state.sushi, s]
+          });
+        });
+      });
+    });
+  };
+
+  eat = sushi => {
+    this.props.onEat(sushi);
+    let foundSush;
+    foundSush = this.state.sushi.find(s => {
+      return s.id === sushi.id;
+    });
+    foundSush.img_url = '';
+  };
+
+  handleClick = e => {
+    this.setState({
+      batchIndex: this.state.batchIndex + 4,
+    });
+  };
+
+  renderSushi = () => {
+    return this.state.sushi.slice(this.state.batchIndex, this.state.batchIndex + 4).map(s => {
+      return (
+        <Sushi
+          id={s.id}
+          name={s.name}
+          price={s.price}
+          img_url={s.img_url}
+          eat={() => this.eat(s)}
+        />
+      );
+    });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <div className="belt">
+          {this.renderSushi()}
+          <MoreButton handleClick={this.handleClick}/>
+        </div>
+      </Fragment>
+    );
+  };
+};
+
+export default SushiContainer;
