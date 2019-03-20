410accf9d5c9f96094ccc412fd9663500c0cc488
davidfloyd91 <davidfloyd91@gmail.com>
2019-03-20 17:03:33 -0400

    Did the things more better

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -3,18 +3,58 @@ import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+  state = {
+    sushi: [],
+    index: 0,
+    eatenSushi: [],
+    bread: 200,
+  };
+
+  componentDidMount() {
+    fetch(API)
+    .then(r => r.json())
+    .then(sushi => {
+      this.setState({ sushi })
+    });
+  };
+
+  eat = sushi => {
+    (this.state.bread >= sushi.price && !this.state.eatenSushi.includes(sushi))
+      ?
+    this.setState({
+      eatenSushi: [...this.state.eatenSushi, sushi],
+      bread: this.state.bread - sushi.price,
+    })
+      :
+    alert('Naw dude rules\'re rules');
+  };
+
+  renderMore = () => {
+    this.setState({
+      index: this.state.index + 4,
+    });
+  };
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          sushi={this.state.sushi}
+          eatenSushi={this.state.eatenSushi}
+          index={this.state.index}
+          more={this.renderMore}
+          eat={sushi => this.eat(sushi)}
+        />
+        <Table
+          plateArray={this.state.eatenSushi}
+          bread={this.state.bread}
+        />
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,49 @@
-import React, { Fragment } from 'react'
-import MoreButton from '../components/MoreButton'
+import React, { Fragment } from 'react';
+import MoreButton from '../components/MoreButton';
+import Sushi from '../components/Sushi';
 
-const SushiContainer = (props) => {
+const SushiContainer = props => {
+  const eat = id => {
+    let eatenSushi = props.sushi.find(s => {
+      return s.id === id;
+    });
+    props.eat(eatenSushi);
+  };
+
+  const renderSushi = () => {
+    return props.sushi.slice(props.index, props.index + 4).map(s => {
+      return (
+          props.eatenSushi.includes(s)
+            ?
+          <Sushi
+          key={s.id}
+          id={s.id}
+          name={s.name}
+          img_url=''
+          price={s.price}
+          eat={id => eat(id)}
+          />
+            :
+          <Sushi
+            key={s.id}
+            id={s.id}
+            name={s.name}
+            img_url={s.img_url}
+            price={s.price}
+            eat={id => eat(id)}
+          />
+      );
+    });
+  };
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {renderSushi()}
+        <MoreButton more={props.more} />
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default SushiContainer
+export default SushiContainer;
